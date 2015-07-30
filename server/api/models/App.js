e429fd7e4eab5977a80a4a3659346b675c1c7a28
Thomas Charlat <thomas.charlat@gmail.com>
2015-07-30 17:28:31 +0200

    New controllers and models

---
+++ server/api/models/App.js
@@ -0,0 +1,24 @@
+/**
+* App.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	name: {
+  		unique: true,
+  		type: 'string',
+  		required: true
+  	},
+  	map: {
+  		model:'map'
+  	},
+  	test: {
+  		type: 'string'
+  	}
+  }
+};
+
---
+++ server/api/models/Map.js
@@ -0,0 +1,23 @@
+/**
+* Map.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+  	login: {
+  		type: 'url'
+  	},
+  	achievement: {
+  		type: 'url'
+  	},
+  	app: {
+  		type: 'json',
+  		model: 'app'
+  	}
+  }
+};
+
